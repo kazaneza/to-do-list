8b5814cde397f6c7db3e05bd5be92ebf2d1813ef
kazaneza <kazanezagentil@gmail.com>
2023-01-28 19:27:22 +0100

    added stae,ui, and user .js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import './style.css';
+import display from './ui.js';
+import './user.js';
+import './state.js';
 
-const taskList = document.querySelector('.container2');
-if (taskList) {
-  const tasks = [
-    { description: 'wash the dishes', completed: false, index: 1 },
-    { description: 'complete To Do list', completed: false, index: 2 },
-  ];
-
-  const populateList = () => {
-    tasks
-      .sort((a, b) => a.index - b.index)
-      .forEach((task) => {
-        const listItem = document.createElement('li');
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        const taskDescription = document.createElement('span');
-        taskDescription.innerHTML = task.description;
-        const taskActions = document.createElement('div');
-        taskActions.innerHTML = '...';
-        listItem.appendChild(checkbox);
-        listItem.appendChild(taskDescription);
-        listItem.appendChild(taskActions);
-        taskList.appendChild(listItem);
-      });
-  };
-
-  document.addEventListener('DOMContentLoaded', populateList);
-}
+document.addEventListener('DOMContentLoaded', display);
